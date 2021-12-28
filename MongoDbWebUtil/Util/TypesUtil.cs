10b37b9613b1c09655c99d8d2fcd95ba8dae9521
Elky (SeongHoon Kim) <elky84@gmail.com>
2021-12-28 21:37:35 +0900

    미사용 Enum 유틸리티 코드 제거

--- MongoDbWebUtil/Util/TypesUtil.cs
+++ MongoDbWebUtil/Util/TypesUtil.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using MongoDbWebUtil.Common;
 
 namespace MongoDbWebUtil.Util
 {
@@ -19,34 +17,6 @@ namespace MongoDbWebUtil.Util
             T[] Arr = (T[])Enum.GetValues(src.GetType());
             int j = Array.IndexOf<T>(Arr, src) + 1;
             return (Arr.Length == j) ? Arr[0] : Arr[j];
-        }
-
-        public static Enum FromDescription(Type type, string description)
-        {
-            foreach (var e in Enum.GetValues(type))
-            {
-                Enum eValue = (Enum)Enum.ToObject(type, e);
-                if (eValue.GetDescription() == description)
-                {
-                    return eValue;
-                }
-            }
-
-            return null;
-        }
-
-        public static Nullable<T> FromDescription<T>(string description) where T : struct
-        {
-            foreach (T e in (T[])Enum.GetValues(typeof(T)))
-            {
-                Enum eValue = (Enum)Enum.ToObject(typeof(T), e);
-                if (eValue.GetDescription() == description)
-                {
-                    return e;
-                }
-            }
-
-            return null;
         }
 
         public static Nullable<T> FromName<T>(string name) where T : struct
